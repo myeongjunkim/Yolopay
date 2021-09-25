36e03a668e9c14b22e981d324058e1c0837d738d
dain-lee <dain9934@gmail.com>
2021-09-25 15:03:19 +0900

    feat: update css

--- Yolopay/mainpage/static/js/calendar.js
+++ Yolopay/mainpage/static/js/calendar.js
@@ -11,42 +11,39 @@ window.addEventListener('DOMContentLoaded', function () {
 
     for (i; i < 32; i++) {
         var result = '';
-        result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+        result = '<li><span>' + i + "</span><img src='' id=" + i + "alt='' /></li>";
         document.getElementById('first-week-1').innerHTML += result;
     }
     i = 1;
     for (i; i < 31; i++) {
         if (i <= 4) {
-            result = "<li onclick='dailyModal(" + i + ");'><span>0" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = "<li onclick='dailyModal(" + i + ");'><span>0" + i + "</span><img src='../static/img/yolo.png' id=" + i + " alt='' /></li>";
             document.getElementById('first-week-2').innerHTML += result;
         } else if (i >= 5 && i <= 9) {
-            result = "<li onclick='dailyModal(" + i + ");'><span>0" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = "<li onclick='dailyModal(" + i + ");'><span>0" + i + "</span><img src='../static/img/yolo.png' id=" + i + " alt='' /></li>";
             document.getElementById('second-week').innerHTML += result;
         } else if (i >= 10 && i <= 11) {
-            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' id=" + i + " alt='' /></li>";
             document.getElementById('second-week').innerHTML += result;
         } else if (i >= 12 && i <= 18) {
-            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' id=" + i + " alt='' /></li>";
             document.getElementById('third-week').innerHTML += result;
         } else if (i >= 19 && i <= 25) {
-            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' id=" + i + " alt='' /></li>";
             document.getElementById('fourth-week').innerHTML += result;
         } else {
-            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = "<li onclick='dailyModal(" + i + ");'><span>" + i + "</span><img src='../static/img/yolo.png' id=" + i + " alt='' /></li>";
             document.getElementById('fifth-week-1').innerHTML += result;
         }
     }
     i = 1;
     for (i; i < 3; i++) {
         if (i <= 2) {
-            result = "<li onclick='dailyModal(" + i + ");'><span>0" + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
+            result = '<li><span>0' + i + "</span><img src='' id=" + i + " alt='' /></li>";
             document.getElementById('fifth-week-2').innerHTML += result;
         }
-        // else {
-        //     result = '<li><span>' + i + "</span><img src='../static/img/yolo.png' alt='' /></li>";
-        //     document.getElementById('sixth-week').innerHTML += result;
-        // }
     }
+    document.getElementById(date).className = 'active-day';
 });
 
 function statsModal() {
